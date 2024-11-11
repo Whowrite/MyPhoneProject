7dd7b5e6b075724642867d6d7cd126eecce8cfc5
Anton Saprykin <saprykinanton10@gmail.com>
2024-11-11 13:48:39 +0200

    commit 7

--- screens/ProfilePage.js
+++ screens/ProfilePage.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Image, Button, StyleSheet, Alert } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
-const ProfilePage = ({navigation, route}) => {
+const ProfilePage = ({ navigation, route }) => {
     const { UserName } = route.params;
+    const [profileImage, setProfileImage] = useState('https://images.squarespace-cdn.com/content/v1/569591ff0ab3771dba3f1ec6/1650383193773-4E38HIVJMG16Q5MHD80A/JD+Solo+by+Todd+V+Wolfson.jpg?format=2500w');
+
+    const pickImage = async () => {
+        // Запит дозволів на доступ до бібліотеки зображень
+        const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+        if (!permissionResult.granted) {
+            Alert.alert("Permission to access the photo library is required!");
+            return;
+        }
+
+        // Відкриття бібліотеки для вибору зображення
+        const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+          });      
+
+        if (!result.canceled) {
+            setProfileImage(result.assets[0].uri);
+        }
+    };
+
     return (
         <View style={styles.container}>
-            <Image style={styles.profileImage} source={{ uri: 'https://images.squarespace-cdn.com/content/v1/569591ff0ab3771dba3f1ec6/1650383193773-4E38HIVJMG16Q5MHD80A/JD+Solo+by+Todd+V+Wolfson.jpg?format=2500w' }} />
+            <Image style={styles.profileImage} source={{ uri: profileImage }} />
             <Text style={styles.name}>{UserName}</Text>
+            <Button title="Change Profile Image" onPress={pickImage} />
             <Button title="Logout" onPress={() => navigation.navigate('LoginPage')} />
             <Button title="Home" onPress={() => navigation.navigate('Home')} />
         </View>
